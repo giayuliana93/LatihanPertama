247a210e5434199774a9822928a545dc9b5e4808
Gia Yuliana <giayuliana3@gmail.com>
2016-03-27 17:53:33 +0700

    Added files via upload

---
+++ MetodeCoba.java
@@ -0,0 +1,23 @@
+public class MetodeCoba {
+
+public static void main (String[] args){
+
+MetodeCoba nama = new MetodeCoba();
+System.out.println(nama.namaSaya());
+nama.pertambahan();
+
+}
+
+//fungsi -> sesuatu yang mengembalikan nilai
+
+public String namaSaya(){
+return "GiaYuliana";
+
+  }
+
+public void pertambahan() {
+int a = 2;
+int b = 3;
+System.out.println(a+b);
+}
+}
---
+++ Perulangan.java
@@ -0,0 +1,11 @@
+public class Perulangan {
+
+public static void main(String args[]){
+       int n; 
+       n=0;
+       while(n<=10){
+       System.out.println(n);
+       n++;
+     }
+   }
+}
