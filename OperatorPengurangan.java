e4671467c409c00e1248f09f4ba6b3a7cc020f2c
Gia Yuliana <giayuliana3@gmail.com>
2016-03-29 11:02:48 +0700

    Added files via upload

---
+++ OperatorPengurangan.java
@@ -0,0 +1,12 @@
+public class OperatorPengurangan{
+
+public static void main(String args[]){
+        int n1,n2,n3;
+        
+        n1=10;
+        n2=5;
+        n3=n1-n2;
+   
+       System.out.println(n1+"-"+n2+"="+n3);
+   }
+}
