aada12a349c3009c4411c9a1112225d33b1eee9b
Gia Yuliana <giayuliana3@gmail.com>
2016-03-29 11:13:34 +0700

    Added files via upload

---
+++ OperatorPenjumlahan.java
@@ -0,0 +1,12 @@
+public class OperatorPenjumlahan{
+
+public static void main(String args[]){
+        int n1,n2,n3;
+        
+        n1=10;
+        n2=10;
+        n3=n1+n2;
+   
+       System.out.println(n1+"+"+n2+"="+n3);
+   }
+}
